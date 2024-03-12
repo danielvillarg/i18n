5b3af61debe4fc69072da7221e5a4d594845f706
Daniel Villar González <69615239+danielvillarg@users.noreply.github.com>
2024-03-12 12:04:33 -0500

    Listos

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,42 @@
 import React from "react";
+import { FormattedDate, FormattedNumber, FormattedMessage } from "react-intl";
 
 const Job = (props) => {
+  const language = (navigator && navigator.language.split(/[-_]/)[0]) || "en";
+
+  const formatSalary = (salary) => {
+    if (language === "es") {
+      return salary === 1 ? "millón" : "millones";
+    } 
+    else {
+      return "million";
+    }
+  };
+
   return (
     <tr>
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        {props.offer.salary}{" "}
+        <FormattedMessage
+          id={formatSalary(props.offer.salary)}
+        />
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year="numeric"
+          month="long"
+          day="numeric"
+          weekday="long"
+        />
+      </td>
+      <td>
+        <FormattedNumber value={props.offer.views} />
+      </td>
     </tr>
   );
 };
--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Job from "./job";
+import { FormattedMessage } from "react-intl";
 
 const JobsList = () => {
+  const [language, setLanguage] = useState("en");
+
+  useEffect(() => {
+    if (navigator && navigator.language) {
+      setLanguage(navigator.language.split(/[-_]/)[0]);
+    }
+  }, []);
+
   const [offers] = useState([
     {
       id: "0001",
@@ -10,6 +19,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      views: 1030400,
     },
     {
       id: "0002",
@@ -18,6 +28,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      views: 25030030,
     },
     {
       id: "0003",
@@ -26,20 +37,22 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      views: 125000,
     },
   ]);
 
   return (
     <div>
-      <table className="table">
-        <thead className="thead-dark">
+      <table className={`table ${language === "es" ? "light" : "dark"}`}>
+        <thead style={{ backgroundColor: language === "es" ? "lightgray" : "darkgray" }}>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col"><FormattedMessage id="Position" /></th>
+            <th scope="col"><FormattedMessage id="Company" /></th>
+            <th scope="col"><FormattedMessage id="Salary" /></th>
+            <th scope="col"><FormattedMessage id="City" /></th>
+            <th scope="col"><FormattedMessage id="PublicationDate" /></th>
+            <th scope="col"><FormattedMessage id="Views" /></th>
           </tr>
         </thead>
         <tbody>
